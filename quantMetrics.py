653f61789bd356e4a14b31aea74b2dfae3321959
David <dkowalczyk135@gmail.com>
2022-08-24 01:15:22 -0400

    Add function declarations for quantitative metrics for bonds

---
+++ quantMetrics.py
@@ -0,0 +1,12 @@
+
+def MacaulayDuration(bond)->float:
+    """Returns the Macaulay Duration of the passed in bond object"""
+    pass
+
+def ModifiedDuration(bond)->float:
+    """Returns the Modified Duration of the passed in bond object"""
+
+def Convexity(bond)->float:
+    """Measures the convexity of the passed in bond object"""
+
+
