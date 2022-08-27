ce84bc0cb852fefc88f130c5b61956870f807053
David <dkowalczyk135@gmail.com>
2022-08-27 11:46:57 -0400

    Add getter methods to Bond class

--- Bond.py
+++ Bond.py
@@ -1,12 +1,38 @@
 
 class Bond:
 
-    def __init__(self, m, cr, fv, ytm=None):
-        self.maturity = m
-        self.cpnRate = cr
-        self.faceValue = fv
-        self.yieldToMaturity = ytm
+    def __init__(self, m, cr, fv, ytm, p):
+        self._maturity = m
+        self._cpnRate = cr
+        self._faceValue = fv
+        self._yieldToMaturity = ytm
+        self._quotedPrice = p
+        self._calculatedPrice = 0
 
-    def setYTM(self, ytm):
-        self.yieldToMaturity = ytm
+    """Getter Methods
+    
+    Retrieve the Bond attributes when called on instance"""
+    def getMaturity(self):
+        return self.maturity
+
+    def getCouponRate(self):
+        return self.cpnRate
+
+    def getFaceVal(self):
+        return self.faceValue
+
+    def getPriceQuoted(self):
+        return self.quotedPrice
+
+    def getYTM(self):
+        return self.yieldToMaturity
+
+    def getCalcPrice(self):
+        """If price was not calcualted indicate so to user when calling this method"""
+        if self._calculatedPrice == 0:
+            return "Price was not calculated! Perform calculation first!"
+        else:
+            return self._calculatedPrice
+
+    
 
