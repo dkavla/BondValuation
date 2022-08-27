3e2abe021a1243c6dd1d842ed4f12fd2a63043bb
David <dkowalczyk135@gmail.com>
2022-08-27 12:15:27 -0400

    Edit method names to follow common practice

--- quantMetrics.py
+++ quantMetrics.py
@@ -1,12 +1,13 @@
 
-def MacaulayDuration(bond)->float:
+def Macaulay_Duration(bond)->float:
     """Returns the Macaulay Duration of the passed in bond object"""
     pass
 
-def ModifiedDuration(bond)->float:
+def Modified_Duration(bond)->float:
     """Returns the Modified Duration of the passed in bond object"""
 
 def Convexity(bond)->float:
     """Measures the convexity of the passed in bond object"""
+
 
 
