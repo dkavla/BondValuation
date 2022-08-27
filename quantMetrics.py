6f65f29c74ad480ef76efb44b3d40a5c46038e90
David <dkowalczyk135@gmail.com>
2022-08-27 12:32:51 -0400

    Add additional methods for calculating other metrics

--- quantMetrics.py
+++ quantMetrics.py
@@ -5,9 +5,17 @@ def Macaulay_Duration(bond)->float:
 
 def Modified_Duration(bond)->float:
     """Returns the Modified Duration of the passed in bond object"""
+    pass
 
 def Convexity(bond)->float:
     """Measures the convexity of the passed in bond object"""
+    pass
 
+def holding_Period_Return(bond)->float:
+    """Returns the holding period return of the passed in bond"""
+    pass
 
+def yield_To_Call(bond, n):
+    """Calculates the yield to call of the passed in bond"""
+    pass
 
