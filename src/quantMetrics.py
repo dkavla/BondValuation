c79a39640b71f85e5f7a972cc8807a10c0cf99fb
David <dkowalczyk135@gmail.com>
2022-10-18 15:00:49 -0400

    Edit calculations for metrics

--- src/quantMetrics.py
+++ src/quantMetrics.py
@@ -6,10 +6,10 @@ def Macaulay_Duration(b: Bond)->float:
     def weight_of_Cpn(t: int, b_price: float)->float:
         """Calculates the weight of each coupon from period t"""
         if t == b.get_Maturity():
-            numer = ( ( (b.get_Coupon_Rate() / 100) * b.get_Face_Val() ) + b.get_Face_Val() ) / pow(1 + (b.get_YTM() / 100), t)
+            numer = ( ( b.get_Coupon_Rate() * b.get_Face_Val() ) + b.get_Face_Val() ) / pow(1 + b.get_YTM(), t)
             return numer / b_price
 
-        numer = ((b.get_Coupon_Rate() / 100) * b.get_Face_Val()) / pow(1 + (b.get_YTM() / 100), t)
+        numer = (b.get_Coupon_Rate() * b.get_Face_Val()) / pow(1 + b.get_YTM(), t)
         return numer / b_price
 
     total_periods = b.get_Maturity()
@@ -24,18 +24,19 @@ def Macaulay_Duration(b: Bond)->float:
 
 def Modified_Duration(b: Bond)->float:
     """Returns the Modified Duration of the passed in bond object"""
-    return round( Macaulay_Duration(b) / (1 + (b.get_YTM() / 100)), 2 )
+    return round( Macaulay_Duration(b) / (1 + b.get_YTM()), 2 )
 
 def Convexity(b: Bond)->float:
     """Measures the convexity of the passed in bond object"""
     def second_factor():
+        
         """Helper function for calculating the second factor"""
         def discount(pmt, y, t):
             """Helper function for discounting coupons"""
             return pmt / pow((1 + y), t)
 
-        cpn = b.get_Coupon_Rate() / 100
-        ytm = b.get_YTM() / 100
+        cpn = b.get_Coupon_Rate()
+        ytm = b.get_YTM()
         total = 0
 
         for i in range(1, b.get_Maturity() + 1):
@@ -43,7 +44,7 @@ def Convexity(b: Bond)->float:
 
         return round(total, 2)
 
-    first_factor = 1 / (b.bond_Price() * pow(1 + (b.get_YTM() / 100), 2))
+    first_factor = 1 / (b.bond_Price() * pow(1 + b.get_YTM(), 2))
     return first_factor * second_factor()
 
 def holding_Period_Return(b: Bond, current_price)->float:
