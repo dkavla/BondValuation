95a53248d4c5faf002b76f5b1dea78c0834f3bc1
David <dkowalczyk135@gmail.com>
2023-06-06 14:10:48 -0400

    Redefine the Duration fucntions and remove holding period return function

--- src/quantMetrics.py
+++ src/quantMetrics.py
@@ -1,54 +1,49 @@
 from Bond import Bond
+from math import pow
 
-def Macaulay_Duration(b: Bond)->float:
-    """Returns the Macaulay Duration of the passed in bond object"""
+def MacaulayDuration(b: Bond) -> float:
+    coupon = b.getCpnRate() * b.getFaceValue
+    compounding = getCompounding(b)
+    totalPeriods = compounding * b.getYearsUntilMaturity()
 
-    def weight_of_Cpn(t: int, b_price: float)->float:
-        """Calculates the weight of each coupon from period t"""
-        if t == b.get_Maturity():
-            numer = ( ( b.get_Coupon_Rate() * b.get_Face_Val() ) + b.get_Face_Val() ) / pow(1 + b.get_YTM(), t)
-            return numer / b_price
-
-        numer = (b.get_Coupon_Rate() * b.get_Face_Val()) / pow(1 + b.get_YTM(), t)
-        return numer / b_price
-
-    total_periods = b.get_Maturity()
-    price = b.bond_Price()
     duration = 0
-    t = 1
-    while t <= total_periods:
-        weight_at_t = weight_of_Cpn(t, price)
-        duration += (t * weight_at_t)
-        t += 1
-    return round(duration, 2)
+    price = b.calculateBondPrice()
 
-def Modified_Duration(b: Bond)->float:
-    """Returns the Modified Duration of the passed in bond object"""
-    return round( Macaulay_Duration(b) / (1 + b.get_YTM()), 2 )
+    for t in range(1, totalPeriods + 1):
+        discountFactor = getDiscountFactor(t, compounding, b.getYieldToMaturity())
 
-def Convexity(b: Bond)->float:
-    """Measures the convexity of the passed in bond object"""
-    def second_factor():
-        
-        """Helper function for calculating the second factor"""
-        def discount(pmt, y, t):
-            """Helper function for discounting coupons"""
-            return pmt / pow((1 + y), t)
+        if t == totalPeriods:
+            pvOfCashFlow = ( (coupon / compounding) + b.getFaceValue()) * discountFactor
+            duration += (t * pvOfCashFlow) / price
+        else:
+            pvOfCashFlow = (coupon / compounding) * discountFactor
+            duration += (t * pvOfCashFlow) / price
 
-        cpn = b.get_Coupon_Rate()
-        ytm = b.get_YTM()
-        total = 0
+    return duration
 
-        for i in range(1, b.get_Maturity() + 1):
-            total += discount(cpn, ytm, i) * (pow(i, 2) + i)
+def ModifiedDuration(b: Bond) -> float:
+    compunding = getCompounding(b)
+    return MacaulayDuration(b) / (1 + (b.getYieldToMaturity / compunding))
 
-        return round(total, 2)
 
-    first_factor = 1 / (b.bond_Price() * pow(1 + b.get_YTM(), 2))
-    return round(first_factor * second_factor(), 2)
+""" Helper Function for the above methods """
+def getCompounding(b: Bond) -> int:
+    if b.getPaymentsPerYear() == 'A':
+        return 1
+    elif b.getPaymentsPerYear() == 'S':
+        return 2
+    else:
+        return 4
 
-def holding_Period_Return(b: Bond, current_price)->float:
-    """Returns the holding period return of the passed in bond"""
-    initial_price = b.bond_Price()
-    cpn = b.get_Coupon_Rate() * b.get_Face_Val()
-    return round((cpn + (current_price - initial_price)) / initial_price, 2)
+def getDiscountFactor(t: int, m: int, ytm: float) -> float:
+    return 1 / pow( (1 + (ytm / m)), t)
+
+
+
+
+
+
+
+
+
+
